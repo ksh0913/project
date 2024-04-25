4738c17f87b62963dcbec2e154b1e29bb6a4a297
ksh0913 <164991158+ksh0913@users.noreply.github.com>
2024-04-26 08:40:08 +0900

    Delete login directory

--- login/src/main/webapp/resources/js/changeToggler.js
+++
@@ -1,8 +0,0 @@
-function changeToggler() {
-  let togglerIcon = document.querySelector(".navbar-toggler-icon");
-  if (togglerIcon.classList.contains("active")) {
-    togglerIcon.classList.remove("active");
-  } else {
-    togglerIcon.classList.add("active");
-  }
-}
